b6ddc518eaaab2a1dddef76fb308ad4ddb582b69
Tanisha-Gupta-16 <tanisha.gupta.00165@gmail.com>
2026-02-04 18:21:15 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Helllllooooo User");
+});
+app.listen(3000, () => {
+  console.log("Server is listening on port 30000");
+});
